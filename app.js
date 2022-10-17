987d7747b5c358e6474840559e4127eb9e9d2f7f
Ignatius Handijoso S <108178271+handijoso@users.noreply.github.com>
2022-10-17 16:42:16 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ toggleBtn.addEventListener("click", () => {
         menuItems[1].style.transform = "translate(-70px, -80px)";
         menuItems[2].style.transform = "translate(-100px, -0px)";
         menuActive = true;
-        toggleBtn.classList.add("active");
+        toggleBtn.classList.add("pop-out-active");
     } else {
         menuItems.forEach((menuItem) => {
             menuItem.style.transform = "translate(0,0)";
         });
         menuActive = false;
-        toggleBtn.classList.remove("active");
+        toggleBtn.classList.remove("pop-out-active");
     }
 });
