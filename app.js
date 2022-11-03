d9906ff4b0b695e02c7a6539cc130c2b42cb60a0
Ignatius Handijoso S <108178271+handijoso@users.noreply.github.com>
2022-11-03 17:52:11 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -16,3 +16,32 @@ toggleBtn.addEventListener("click", () => {
         toggleBtn.classList.remove("pop-out-active");
     }
 });
+
+
+const navbarContainer = document.querySelector("#navbar-container");
+
+function navbarBackground() {
+    let windowPosition = window.scrollY;
+
+    if (windowPosition > 150) {
+        navbarContainer.classList.add("show-background")
+    }
+    else {
+        navbarContainer.classList.remove("show-background")
+    }
+}
+window.addEventListener("scroll", navbarBackground)
+
+const observer = new IntersectionObserver((enteries) => {
+    enteries.map((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show")
+        }
+        else {
+            entry.target.classList.remove("show")
+        }
+    })
+})
+
+const animateContents = document.querySelectorAll(".content");
+animateContents.forEach((el) => observer.observe(el));
